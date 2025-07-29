9f8d900a00db6ac80a0fbeba0b112d3d3fe6e58f
Kelechi Opara <kaycopara911@gmail.com>
2025-07-29 09:32:35 +0100

    adding to the clone

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,13 @@
 from flask import Flask
+from app.auth.routes import auth
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'your-secret-key'
+    app.secret_key = 'uber_clone_secret'
 
     from .routes import main
-    app.register_blueprint(main)
+    from .auth import auth
 
+    app.register_blueprint(main)
+    app.register_blueprint(auth, url_prefix='/auth')
     return app
